d1ad2b15a3569dc7615f8205fcd26d60e36645d3
K Todd-Brown <ktoddbrown@ufl.edu>
2024-04-24 17:14:30 -0400

    adding testing for checkAnnotations

--- R/checkAnnotations.R
+++ R/checkAnnotations.R
@@ -1,8 +1,20 @@
 checkAnnotations <- function(filename){
   
-  expectedHeaders <- c("table_id", "column_id", "of_variable", "is_type", "with_entry" )
+  expectedHeaders <- c("table_id", #Table id should match the basename of the csv file
+                       "column_id", #This should match the exact text of the headers
+                       "of_variable", #variables should match how the orginal file talks about data. For example if bulk density is reported as both fine earth and whole the variable might be `fine_bulk_density` and `whole_bulk_densith`. Best practice is to use `_` to seperate words and lower case.
+                       "is_type", #should be one of the control vocabulary below
+                       "with_entry" #should be either the value/entry associated with this time and varible OR a `--` to indicate a reference to the described data table.
+                       )
   
-  valid_is_type <- c('identifier', 'value', 'unit', 'method', 'note',
+  data_ref <- '--' #string for reference to annotated data to appear in the `with_entry` column
+  
+  valid_is_type <- c('identifier',
+                     'value',
+                     'unit',
+                     'method',
+                     'note',
+                     'control_vocabulary',
                      'standard_deviation')
   
   annotation.df <- read_csv2(filename, 
@@ -13,7 +25,7 @@ checkAnnotations <- function(filename){
   if(!all(expectedHeaders %in%
          names(annotation.df))){
     stop(paste('annotation file missing expected names: ', 
-               setdiff(expectedHeaders, names(annotation.df))))
+               paste0(setdiff(expectedHeaders, names(annotation.df)), collapse = ', ')))
   }
   
   #check is_type elements
@@ -22,4 +34,17 @@ checkAnnotations <- function(filename){
                setdiff(unique(annotation.df$is_type), valid_is_type)))
   }
   
+  check_self_ref <- annotation.df %>%
+    filter(with_entry == data_ref) %>%
+    mutate(across(.cols = everything(),.fns = function(xx){
+      xx[xx == data_ref] <- NA
+      return(xx)
+    }))
+  
+  #check that only table-columns have the '--'
+  if(!(any(is.na(check_self_ref$column_id)))){
+    stop('no reference to data table premitted without specifying the column_id')
+  }
+  
+  return(0)
 }
---
+++ tests/testthat/test-checkAnnotations.R
@@ -0,0 +1,13 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+badHeaders <- 
+'column_name; table_name; of_variable; is_type; with_entry
+ ; ; ; ;'
+
+test_that('checking bad headers', {
+  expect_error(checkAnnotations(badHeaders))
+  })
+
+
